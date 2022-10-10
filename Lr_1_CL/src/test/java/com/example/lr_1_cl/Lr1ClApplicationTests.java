caac589b11a214f26d47936402964f7713ce1e3e
DezertRose <Dezertrouze@gmail.com>
2022-10-10 21:38:53 +0300

    command line aplication

---
+++ Lr_1_CL/src/test/java/com/example/lr_1_cl/Lr1ClApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lr_1_cl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lr1ClApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
