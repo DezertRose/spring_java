e9c4980840b5fae048256541755477546faed498
DezertRose <Dezertrouze@gmail.com>
2022-10-10 21:07:23 +0300

    start

---
+++ Lr1_v0/src/main/java/com/example/lr1_v0/Lr1V0Application.java
@@ -0,0 +1,20 @@
+package com.example.lr1_v0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@SpringBootApplication
+public class Lr1V0Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lr1V0Application.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
+    }
+
+}
