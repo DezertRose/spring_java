809018bf85295e0549ec38d922e12ac42473311a
DezertRose <Dezertrouze@gmail.com>
2022-10-11 08:47:28 +0300

    team HTML-pages

--- Lr1_Web/src/main/java/com/example/lr1_web/Web_Controller.java
+++ Lr1_Web/src/main/java/com/example/lr1_web/Web_Controller.java
@@ -2,14 +2,34 @@ package com.example.lr1_web;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class Web_Controller {
-    @GetMapping("/")
+     /*@GetMapping("/")
     @ResponseBody
-    public String hello(){
+    public String team(Model model){
+        return"teamPage";
+    }
+   public String hello(){
         return "Hello world!";
+    }*/
+
+    @RequestMapping("/team")
+    public String teamPage(Model model){
+        return"teamPage";
+    }
+
+    @RequestMapping("/myPage1")
+    public String DariaShnyrkova(Model model){
+        return"DariaShnyrkovaPage";
+    }
+
+    @RequestMapping("/myPage2")
+    public String DariaOvcharenko(Model model){
+        return"DariaOvcharenkoPage";
     }
 }
